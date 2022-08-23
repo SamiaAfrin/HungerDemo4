b34c83bf73d26c6bf61051d2e0b9cd72e43e4555
HP <HP@TONMOY>
2022-08-23 10:58:29 +0600

    Add project files.

---
+++ Controllers/HomePageTestController.cs
@@ -0,0 +1,23 @@
+﻿using HungerDemo4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HungerDemo4.Controllers
+{
+    public class HomePageTestController : Controller
+    {
+        
+        HungerDb db = new HungerDb();
+        // GET: HomePageTest
+        public ActionResult Index()
+        {
+            List<Product> products = db.Products.ToList<Product>();
+            return View(products);
+            
+
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HungerDemo4.MvcApplication" Language="C#" %>
